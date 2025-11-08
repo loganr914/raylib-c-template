6005a3e754e1af48e47cf243bcc2999330aeb700
Logan Rosenthal <loganrosenthal1@gmail.com>
2025-11-08 14:41:38 -0500

    Expanded README and broke project

--- README.md
+++ README.md
@@ -1,9 +1,35 @@
-# Raylib Template
+# Raylib C Template
 
-## Quick Start
+### [tsoding/raylib-template](https://github.com/tsoding/raylib-template/) with my personal preference of additions
+
+## Adaptive integer scaling
+
+This template has an integer scaling system, like you'd see with emulators of low resolution systems.
+
+## Compiling and running your game
+
+Compile ```nob.c``` by running:
 
 ```console
 $ cc -o nob nob.c
+```
+
+Then, run ```nob``` to compile ```main```:
+
+```console
 $ ./nob
+```
+
+Now, you can run your project with:
+
+```console
 $ ./main
 ```
+
+## Congratulations!
+
+You are now an independent game developer! 
+
+I hope you make something great
+
+Don't stay up too late!
--- main.c
+++ main.c
@@ -1,12 +1,37 @@
 #include <raylib.h>
 
+const int renderWidth = 640;
+const int renderHeight = 480;
+
+
+
 int main(void)
 {
+    SetConfigFlags(FLAG_WINDOW_TRANSPARENT);
     InitWindow(800, 600, "Raylib Template");
+
+    RenderTexture2D canvas = LoadRenderTexture(renderWidth, renderHeight);
+    
     SetTargetFPS(60);
+
     while (!WindowShouldClose()) {
+        int windowWidth = GetRenderWidth();
+        int windowHeight = GetRenderHeight();
+
+
+
+
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(BLACK);
+        BeginTextureMode(canvas);
+
+        
+
+
+        DrawText("HELLO RAYLIB", 0, 0, 20, WHITE);
+
+        DrawCircleV((Vector2) {50, 50}, 20, WHITE);
+        EndTextureMode();
         EndDrawing();
     }
     CloseWindow();
